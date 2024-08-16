0b8474e02c758833fb74f4aa045ed68934d654e9
ARTHURGOLDWAY25 <galexport08@gmail.com>
2024-08-16 22:38:18 +0200

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="script.php" method="post">
+    <label for="first_name">First Name</label>
+    <input type="text" name="first_name" placeholder="First Name"> <br><br>
+
+    <label for="last_name">Last Name</label>
+    <input type="text" name="last_name" placeholder="Last Name"> <br><br>
+
+    <label for="email">Email</label>
+    <input type="email" name="email" placeholder="Email"> <br><br>
+
+    <button type="submit">Submit</button>
+</form>
+    
+</body>
+</html>
